c79666016e75151e1f94d855bbbc9e723471b7bc
rdjames1996 <robjames@roberts-air.lan>
2023-03-11 15:02:42 -0800

    adding VacationPy

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -5,4 +5,4 @@ weather_api_key = "17e1d19f5230ea5f0405489ec629c4ec"
 geoapify_key = "14d4327a79404017b23abe82f6ff384c"
 
 # Google API key
-g_key = "AIzaSyDV-3dVdjcOniR9n7Ms6jeN7OIINMiTDXc"
+gmaps_key = "AIzaSyDV-3dVdjcOniR9n7Ms6jeN7OIINMiTDXc"
